a4ea2e23e8d3be0fa804c1dee1124f5052931541
Dylan R. Oesch-Emmel <doesch01@vm-hw01.eecs.tufts.edu>
2022-04-01 16:07:28 -0400

    new files for refactoring orders.py

---
+++ order_generator.py
@@ -0,0 +1,9 @@
+"""
+  order_generator.py
+  Description: The driver for producing orders and adding them to the order
+               queue during the game. 
+  Authors: Dylan Oesch-Emmel
+"""
+
+def order_generator():
+    return
---
+++ order_list.py
@@ -0,0 +1,11 @@
+"""
+  order_list.py
+  Description: A thread-safe queue abstraction for orders 
+  Authors: Dylan Oesch-Emmel
+"""
+
+class OrderList:
+   def __init__(self):
+        None
+
+
