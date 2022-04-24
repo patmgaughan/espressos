12cd29890fcf75de8f759330a373ebea68ac0ff9
Dylan R. Oesch-Emmel <doesch01@vm-hw03.eecs.tufts.edu>
2022-04-24 14:26:27 -0400

    order_generator() sends topfive orders to server

--- order_generator.py
+++ order_generator.py
@@ -4,6 +4,8 @@
                queue during the game. 
   Authors: Dylan Oesch-Emmel
 """
+import asyncio
+import websockets
 import time 
 import sys
 
@@ -11,10 +13,11 @@ from order import Order
 from order_list import OrderList
 from pizza import Pizza
 
-def order_generator(difficulty, order_list, game_over):
-   
-    # difficulty sets the rate and rate cap of orders 
-    # TODO: fine tune the rates and caps
+
+# choose_difficulty(difficulty)
+# Returns:  order rate, the decay rate, and the rate cap
+# Purpose:  Return the difficulty settings for order generation 
+def choose_difficulty(difficulty):
     if difficulty == "hard":
         order_rate = 30
         decay_rate = 0.9
@@ -31,23 +34,45 @@ def order_generator(difficulty, order_list, game_over):
         order_rate = 45
         decay_rate = 0.97
         rate_cap = 15
-       
+
+    return order_rate, decay_rate, rate_cap
+
+
+# order_generator()
+# Returns:  nothing, void 
+# Purpose:  generate pizza orders and send them
+#           to the server 
+async def order_generator():
+    assert len(sys.argv) == 2
+
+    uri = "ws://localhost:8765"
+    difficulty = sys.argv[1]
+    order_rate, decay_rate, rate_cap = choose_difficulty(difficulty)      
+
     # need start time so build_pizza can create more complex pizzas as 
     # game progresses  
     start_time = time.time()
 
-    # loop for order making
-    # TODO: figure out way to interrupt thread (exceptions doesn't work)
-    #       maybe using multiprocessing and pipes??
-    while not game_over.is_set():
+    order_list = OrderList()
+
+    while True:
         pizza = Order.build_pizza(start_time) 
         order = Order(pizza)
         order_list.add(order)
-
-        time.sleep(order_rate) 
-
+        
+        # make sure the order_rate does not go below the rate cap
         order_rate = order_rate * decay_rate
         if order_rate < rate_cap:
             order_rate = rate_cap
         
-    
+        top_five_list = order_list.topFive() 
+        # must convert list into one contiguous string to send over websocket
+        top_five_string = "".join(top_five) 
+        async with websockets.connect(uri) as websocket:
+            await websocket.send(top_five_string)
+
+        time.sleep(order_rate) 
+
+
+if __name__ == "__main__":
+    asyncio.run(order_generator())
