0b1830e4e905045cdb3a61543594610752cef711
Jackson Clayton <jackson.clayton@tufts.edu>
2022-04-25 11:47:30 -0400

    merge with orders and add threadsafe counter

---
+++ threadsafe_counter.py
@@ -0,0 +1,23 @@
+import threading
+
+class ThreadsafeCounter:
+    def __init__(self, start = 0):
+        self.lock = threading.Lock()
+        self.count = start
+
+    def __str__(self):
+        with self.lock:
+            return self.count
+
+    def add(self, i):
+        with self.lock:
+            self.count += i
+            return self.count
+
+    def get(self):
+        with self.lock:
+            return self.count
+
+    def increment(self):
+        return self.add(1)
+
--- websock_server.py
+++ websock_server.py
@@ -4,10 +4,13 @@ import asyncio
 import argparse
 import websockets
 import threading
+import time
 from pprint import pprint
 from player_ops import CMND_TO_FUNC
 from order_list import OrderList
 from order_generator import order_generator
+from order import Order
+from threadsafe_counter import ThreadsafeCounter
 
 
 import kitchen
@@ -26,11 +29,11 @@ async def server(kitch, clients, start_pos,
 
     if type == "input":
 
-        async with lock:
-            player = cook.Cook(kitch, start_pos, start_pos, "player")
-            start_pos += 1
+        # TODO: add option to give username
 
-        pprint(globals())
+        async with lock:
+            pos = start_pos.increment()
+            player = cook.Cook(kitch, pos, pos, "player")
 
         for ws in clients:
             await ws.send(str(kitch))
@@ -61,7 +64,7 @@ async def server(kitch, clients, start_pos,
                         pizza = player.emptyHands()
                         succ, msg = order_list.fulfillOrder(pizza)
                         if succ:
-                            completed_orders += 1
+                            completed_orders.increment()
                         else:
                             player.give(pizza)
                 elif (resp == "get_"):
@@ -69,7 +72,6 @@ async def server(kitch, clients, start_pos,
                 else:
                     func()
 
-                expired_orders += order_list.removeExpired()
 
                 print(stringGame(player, kitch, order_list, (completed_orders, expired_orders)))
             for ws in clients:
@@ -82,11 +84,16 @@ async def server(kitch, clients, start_pos,
         while True:
             await asyncio.sleep(1)
 
+    if type == "reprint":
+        while True:
+            await asyncio.sleep(5)
+            for ws in clients:
+                await ws.send(stringGame(player, kitch, order_list, (completed_orders, expired_orders)))
 
 def stringGame(player, kitchen, order_list, order_counts):
     board = [""] * (kitchen.HEIGHT + 1)
 
-    board[0] = player.inventory()
+    board[0] = player.inventory() if player is not None else ""
 
     for i in range(kitchen.HEIGHT):
         board[i + 1] = kitchen.getRow(i)
@@ -124,10 +131,10 @@ async def main():
     kitch = kitchen.Kitchen()
     kitch.setUp()
     clients = []
-    start_pos = 1
+    start_pos = ThreadsafeCounter()
     order_list = OrderList()
-    completed_orders = 0
-    expired_orders = 0
+    completed_orders = ThreadsafeCounter()
+    expired_orders = ThreadsafeCounter()
     game_over = threading.Event()
     lock = asyncio.Lock()
 
@@ -135,15 +142,75 @@ async def main():
     fun = lambda ws: server(kitch, clients, start_pos, order_list, completed_orders,
                             expired_orders, game_over, lock, ws)
 
-    #TODO: put this somewhere else so it doesnt run before people connect
-    order_thread = threading.Thread(target=order_generator,
-                                    args=("hard", order_list, game_over),
-                                    daemon=True)
-    order_thread.start()
     print("started order thread")
 
-    async with websockets.serve(fun, args.i, args.p, ping_interval=None):
-        await asyncio.Future()  # run forever
+    async def run_server():
+        print("running server")
+        async with websockets.serve(fun, args.i, args.p, ping_interval=None):
+            await asyncio.Future()  # run forever
+
+    async def run_orders(clients, expired_orders):
+        print("running orders")
+
+        # choose_difficulty(difficulty)
+        # Returns:  order rate, the decay rate, and the rate cap
+        # Purpose:  Return the difficulty settings for order generation
+        def choose_difficulty(difficulty):
+            if difficulty == "hard":
+                order_rate = 30
+                decay_rate = 0.9
+                rate_cap = 5
+
+
+            elif difficulty == "medium":
+                order_rate = 45
+                decay_rate = 0.94
+                rate_cap = 10
+
+            # make easy the fall-through case
+            else:
+                order_rate = 45
+                decay_rate = 0.97
+                rate_cap = 15
+
+            return order_rate, decay_rate, rate_cap
+
+        while True:
+            order_rate, decay_rate, rate_cap = choose_difficulty("hard")
+
+            # need start time so build_pizza can create more complex pizzas as
+            # game progresses
+            start_time = time.time()
+
+            while True:
+                pizza = Order.build_pizza(start_time)
+                order = Order(pizza)
+                order_list.add(order)
+
+                # make sure the order_rate does not go below the rate cap
+                order_rate = order_rate * decay_rate
+                if order_rate < rate_cap:
+                    order_rate = rate_cap
+
+                expired_orders.add(order_list.removeExpired())
+
+                print(stringGame(None, kitch, order_list, (completed_orders, expired_orders)))
+
+                for ws in clients:
+                    await ws.send(stringGame(None, kitch, order_list, (completed_orders, expired_orders)))
+                await asyncio.sleep(order_rate)
+
+
+    # task1 = asyncio.create_task()
+    # task2 = asyncio.create_task()
+    print("tasks created")
+    await asyncio.gather(run_orders(clients, expired_orders), run_server())
+
+
+
+
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
